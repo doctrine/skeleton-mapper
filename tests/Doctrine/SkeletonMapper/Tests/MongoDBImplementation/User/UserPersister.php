434b37f183728a851f12cce9cc7e0216327dc605
Jonathan H. Wage <jonwage@gmail.com>
2015-03-31 00:06:52 +0000

    Missed something.

--- tests/Doctrine/SkeletonMapper/Tests/MongoDBImplementation/User/UserPersister.php
+++ tests/Doctrine/SkeletonMapper/Tests/MongoDBImplementation/User/UserPersister.php
@@ -2,7 +2,6 @@
 
 namespace Doctrine\SkeletonMapper\Tests\MongoDBImplementation\User;
 
-use Doctrine\SkeletonMapper\Persister\ObjectAction;
 use Doctrine\SkeletonMapper\Persister\MongoDBObjectPersister;
 
 class UserPersister extends MongoDBObjectPersister
@@ -10,18 +9,5 @@ class UserPersister extends MongoDBObjectPersister
     public function getClassName()
     {
         return 'Doctrine\SkeletonMapper\Tests\Model\User';
-    }
-
-    public function executeObjectAction(ObjectAction $objectAction)
-    {
-        $object = $objectAction->getObject();
-
-        switch ($objectAction->getName()) {
-            case 'register':
-                $object->setPassword(md5($object->getPassword()));
-            break;
-        }
-
-        $objectAction->setResult(array('success' => true));
     }
 }
