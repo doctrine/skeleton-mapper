337c6ef80f91b38b90161f7837608738570aa8a2
Jonathan H. Wage <jonwage@gmail.com>
2015-04-01 01:54:09 +0000

    Fix return type.

--- lib/Doctrine/SkeletonMapper/ObjectManager.php
+++ lib/Doctrine/SkeletonMapper/ObjectManager.php
@@ -218,7 +218,7 @@ class ObjectManager implements ObjectManagerInterface
      *
      * @param string $className
      *
-     * @return \Doctrine\Common\ObjectRepository\ObjectRepositoryInterface
+     * @return \Doctrine\Common\Persistence\ObjectRepository
      */
     public function getRepository($className)
     {
