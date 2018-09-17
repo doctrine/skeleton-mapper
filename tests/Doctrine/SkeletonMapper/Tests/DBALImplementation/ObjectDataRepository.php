6a601d561c19b0c8ec1f4e8dcf86c66982ed25cc
Jonathan H. Wage <jonwage@gmail.com>
2018-09-17 21:10:26 +0100

    Remove example implementations from the core.

--- tests/Doctrine/SkeletonMapper/Tests/DBALImplementation/ObjectDataRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Doctrine\SkeletonMapper\Tests\DBALImplementation;
-
-use Doctrine\SkeletonMapper\DataRepository\DBALObjectDataRepository;
-
-class ObjectDataRepository extends DBALObjectDataRepository
-{
-}
--- tests/Doctrine/SkeletonMapper/Tests/HttpImplementation/ObjectPersister.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Doctrine\SkeletonMapper\Tests\HttpImplementation;
-
-use Doctrine\SkeletonMapper\Persister\HttpObjectPersister;
-
-class ObjectPersister extends HttpObjectPersister
-{
-}
