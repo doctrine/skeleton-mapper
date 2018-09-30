72e6abbfb4a2c5dac4b29cae7712d157fa48fc83
Jonathan H. Wage <jonwage@gmail.com>
2018-09-30 06:45:45 +0100

    Use instantiator to create objects without invoking __construct

--- lib/Doctrine/SkeletonMapper/ObjectFactory.php
+++ lib/Doctrine/SkeletonMapper/ObjectFactory.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\SkeletonMapper;
 
-use ReflectionClass;
-use const PHP_VERSION_ID;
-use function sprintf;
-use function strlen;
-use function unserialize;
+use Doctrine\Instantiator\Instantiator;
 
 /**
  * Class for creating object instances without
@@ -16,40 +12,19 @@ use function unserialize;
  */
 class ObjectFactory
 {
-    /** @var object[] */
-    private $prototypes = [];
+    /** @var Instantiator */
+    private $instantiator;
 
-    /** @var ReflectionClass[] */
-    private $reflectionClasses = [];
+    public function __construct()
+    {
+        $this->instantiator = new Instantiator();
+    }
 
     /**
      * @return object
      */
     public function create(string $className)
     {
-        if (! isset($this->prototypes[$className])) {
-            if ($this->isReflectionMethodAvailable()) {
-                $this->prototypes[$className] = $this->getReflectionClass($className)
-                    ->newInstanceWithoutConstructor();
-            } else {
-                $this->prototypes[$className] = unserialize(sprintf('O:%d:"%s":0:{}', strlen($className), $className));
-            }
-        }
-
-        return clone $this->prototypes[$className];
-    }
-
-    protected function isReflectionMethodAvailable() : bool
-    {
-        return PHP_VERSION_ID === 50429 || PHP_VERSION_ID === 50513 || PHP_VERSION_ID >= 50600;
-    }
-
-    private function getReflectionClass(string $className) : ReflectionClass
-    {
-        if (! isset($this->reflectionClasses[$className])) {
-            $this->reflectionClasses[$className] = new ReflectionClass($className);
-        }
-
-        return $this->reflectionClasses[$className];
+        return $this->instantiator->instantiate($className);
     }
 }
